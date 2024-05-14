d48dae8fa7c731a2885002f056c1e363207bd2bd
GG-Balder <154797555+GG-Balder@users.noreply.github.com>
2024-05-14 11:47:55 +0800

    fix: fix eslint error

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { createThirdwebClient } from "thirdweb";
 import { ConnectButton, ThirdwebProvider, } from "thirdweb/react";
 import { createWallet, inAppWallet } from "thirdweb/wallets";
-const metamaskWallet = createWallet("io.metamask");
 const client = createThirdwebClient({ clientId: "5007ba53ef0272dcc52d7e514297d4b1" });
 const wallets = [
-  metamaskWallet,
-  inAppWallet({
-    providers: ["email", "facebook", "apple", "google"],
-  }),
+  inAppWallet(),
+  createWallet("io.metamask"),
+  createWallet("com.coinbase.wallet"),
+  createWallet("me.rainbow")
 ];
 
 export default function App() {
